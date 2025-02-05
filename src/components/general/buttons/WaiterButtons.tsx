b816268e6cc657ec09b3875e6cd319a2c15c8fbc
Yeyeando <sercacsal2@gmail.com>
2025-02-05 16:24:50 +0000

    sistema de ayuda implementado con css

--- src/components/general/buttons/WaiterButtons.tsx
+++ src/components/general/buttons/WaiterButtons.tsx
@@ -1,28 +1,29 @@
-
 import { useNavigate } from "react-router-dom";
-import "./waiter-buttons.css"
+import "./waiter-buttons.css";
 const WaiterButtons: React.FC = () => {
-    const navigate = useNavigate();
-  
-    const handleNavigation = (path: string) => {
-      navigate(path);
-    };
-  
-    return (
-      <div className="button-container">
-        <button
-          className="nav-button"
-          onClick={() => handleNavigation("/")}
-        >
-          Take order
-        </button>
-        <button
-          className="nav-button"
-          onClick={() => handleNavigation("/PickUp")}
-        >
-          Pick up
-        </button>
-      </div>
-    );
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
   };
+  const openHelp = () => {
+    window.open("/help/Realizarcomanda.html", "_blank");
+  };
+  return (
+    <div className="button-container">
+      <button className="nav-button" onClick={() => handleNavigation("/")}>
+        Take order
+      </button>
+      <button className="nav-button" onClick={() => openHelp()}>
+        help
+      </button>
+      <button
+        className="nav-button"
+        onClick={() => handleNavigation("/PickUp")}
+      >
+        Pick up
+      </button>
+    </div>
+  );
+};
 export default WaiterButtons;
